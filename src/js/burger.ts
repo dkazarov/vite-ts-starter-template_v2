9aae9f9a8be91e5b1a042bf228f920b2d2777c3d
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-02-03 13:55:16 +0200

    header: add burger menu

--- src/js/burger.ts
+++ src/js/burger.ts
@@ -1,10 +1,14 @@
 const burgerBtn = document.querySelector('.burger') as HTMLButtonElement;
 const burgerLine = document.querySelector('.burger__lines') as HTMLSpanElement;
 
+const burgerNav = document.querySelector('.nav__list') as HTMLElement;
+
 const initBurger = () => {
 	burgerBtn.addEventListener('click', () => {
 		burgerLine.classList.toggle('burger__line--hide');
 		burgerBtn.classList.toggle('burger--transform');
+
+		burgerNav.classList.toggle('burger__init');
 	});
 };
 
