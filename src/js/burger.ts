00f4765e75c554efec2e411aa1a62afda8219b0c
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-02-03 15:38:43 +0200

    finish

--- src/js/burger.ts
+++ src/js/burger.ts
@@ -1,9 +1,8 @@
 const burgerBtn = document.querySelector('.burger') as HTMLButtonElement;
 const burgerLine = document.querySelector('.burger__lines') as HTMLSpanElement;
-
 const burgerNav = document.querySelector('.nav__list') as HTMLElement;
 
-const initBurger = () => {
+const initBurger = (): void => {
 	burgerBtn.addEventListener('click', () => {
 		burgerLine.classList.toggle('burger__line--hide');
 		burgerBtn.classList.toggle('burger--transform');
